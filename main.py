27af41651029d70bbbf11aa2d9c944d1c19b2147
Brian <brianrik911@gmail.com>
2023-11-12 22:31:09 +0200

    old version

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pandas as pd
-import lzw3
-import matplotlib.pyplot as plt
+
 
 def lzw_compress(input_string):
     # Initialize dictionary with individual characters
@@ -23,7 +22,7 @@ def lzw_compress(input_string):
     if current_string:
         compressed.append(dictionary[current_string])
 
-    return compressed, dictionary
+    return compressed
 
 def lzw_decompress(compressed_data):
     dictionary = {i: chr(i) for i in range(256)}
@@ -83,31 +82,5 @@ if __name__ == '__main__':
     # Calculate the total sales for each product group
     mean_redundancy = grouped['redundancy'].mean()
     stds_redundancy = grouped['redundancy'].std()
-
-    original_seq_counts = df.groupby(['gene', 'clean_seq']).size().reset_index(name='count')
-
-    # for i, sequence in enumerate(df['clean_seq']):
-        # compressed_sequence, redundancy = LZW(sequence)
-        # compressed_sequence_str = ' '.join(map(str, compressed_sequence))
-        # print(f"Gene {df['gene'].iloc[i]}")
-        # print(f"Original Sequence: {sequence}")
-        # print(f"Encoded Sequence: {compressed_sequence_str}")
-        # print(f"Dictionary: {lzw_compress(sequence)[1]}")
-        # print(f"Redundancy: {redundancy}\n")
-
-    # Create a figure and axes for plotting
-    for gene, group in grouped:
-        plt.figure(figsize=(12, 6))
-        plt.title(f'Top 10 Original Sub-Sequences for Gene {gene}')
-        plt.xlabel('Original Sub-Sequence')
-        plt.ylabel('Frequency')
-
-        # Select the top 10 most frequent original sub-sequences for the current gene
-        top_10_sequences = original_seq_counts[original_seq_counts['gene'] == gene].nlargest(10, 'count')
-
-        # Plot a bar chart for the top 10 sequences
-        plt.bar(top_10_sequences['clean_seq'], top_10_sequences['count'])
-
-        plt.xticks(rotation=45)
-        plt.tight_layout()
-        plt.show()
+    # Save or display the modified DataFrame
+    print(df)
